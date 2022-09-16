a47d157810f62af4bcf02721a5ce0e20e6576a39
jennakim1007 <jennakim1007@gmail.com>
2022-09-16 16:40:27 +0900

    continue
    반복제어를 위한 키워드 continue 연습

---
+++ javaproject/FirstJava/src/chapter04/ContinueTest.java
@@ -0,0 +1,24 @@
+package chapter04;
+
+public class ContinueTest {
+
+	public static void main(String[] args) {
+		
+		// 1~9 사이의 홀수만 출력해보자.
+		
+		int num = 0;
+		
+		while(++num<10) {
+			
+			if(num%2==0) {
+				continue;
+			}
+			
+			System.out.println(num);
+			// num++; 
+			
+		}
+				
+	}
+
+}
