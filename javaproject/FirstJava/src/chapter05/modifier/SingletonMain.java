487f27457df640fc4fd17b984e89eb89080f6f27
jennakim1007 <jennakim1007@gmail.com>
2022-09-20 15:07:36 +0900

    싱글톤
    싱글톤 연습 주석추가

--- javaproject/FirstJava/src/chapter05/modifier/SingletonMain.java
+++ javaproject/FirstJava/src/chapter05/modifier/SingletonMain.java
@@ -7,7 +7,7 @@ public class SingletonMain {
 			Singleton s = null;
 			// s= new Singleton(); // private 상태 -> 보이지 않음 
 			
-			s= Singleton.getInstance();
+			s= Singleton.getInstance(); // 싱글톤 패턴
 			
 			
 		}
