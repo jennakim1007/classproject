6b92e9b97b6f7c9c643ae9c40105c5719408ea0c
jennakim1007 <jennakim1007@gmail.com>
2022-09-20 15:08:01 +0900

    chapter05.exam
    chapter05.exam 예제풀이추가 접근제어자 사용

---
+++ javaproject/FirstJava/src/chapter05/exam/MemberMain2.java
@@ -0,0 +1,28 @@
+package chapter05.exam;
+
+public class MemberMain2 {
+
+	public static void main(String[] args) {
+
+		Member2 m1 = new Member2("손흥민", // 이름
+				"010-1111-2222", // 전화번호
+				"축구", // 전공
+				1, // 학년
+				"son@gmail.com", // 이메일
+				20000101, // 생일
+				"런던" // 주소
+		);
+
+		m1.showMemberInfo();
+
+		Member2 m2 = new Member2("손흥민", // 이름
+				"010-1111-2222", // 전화번호
+				"축구", // 전공
+				1, // 학년
+				"son@gmail.com" // 이메일
+		);
+		m2.showMemberInfo();
+
+	}
+
+}
