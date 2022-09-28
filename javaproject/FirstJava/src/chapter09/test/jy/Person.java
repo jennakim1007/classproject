e2389ce89ff60c006e27b25318f9504b3fc8560e
jennakim1007 <jennakim1007@gmail.com>
2022-09-28 23:28:43 +0900

    exam09
    exam09

--- javaproject/FirstJava/src/chapter09/test/jy/Person.java
+++ javaproject/FirstJava/src/chapter09/test/jy/Person.java
@@ -1,9 +1,10 @@
 package chapter09.test.jy;
 
+import chapter09.exam.Person;
+
 public class Person {
 	
 	// Person 클래스의 equals() 메소드를 오버라이딩해서 주민등록번호가 같으면 인스턴스로 판별하는 프로그램을 만들어봅시다. 
-
 	
 	String name;
 	String personNumber;
@@ -15,20 +16,25 @@ public class Person {
 
 	@Override
 	public boolean equals(Object obj) {
-		if(obj != null && obj instanceof Person) {
-			
 		
+		boolean result = false;
+
+		if(obj != null && obj instanceof Person) {
+			Person person = (Person)obj;
+			result = this.personNumber.equals(person.personNumber);
 		}
 		
-		return true;
+		return result;
 	}
-	
-	
 	
 	public static void main(String[] args) {
 		
+		Person p1 = new Person("김나나", "000000-1000000");
+		Person p2 = new Person("김다다", "000000-1000000");
+		Person p3 = new Person("김나나", "000000-2000000");
 		
-		
+		System.out.println("p1과 p2는 같은 사람인가? => " + p1.equals(p2));
+		System.out.println("p1과 p3는 같은 사람인가? => " + p1.equals(p3));
 		
 	}
 	
