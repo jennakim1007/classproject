b47bcbb942ec5db794b46c36000cabd4db6dc7ae
jennakim1007 <jennakim1007@gmail.com>
2022-10-07 12:32:28 +0900

    thread

---
+++ javaproject/FirstJava/src/chapter13/MyThread2.java
@@ -0,0 +1,21 @@
+package chapter13;
+
+public class MyThread2 implements Runnable {
+
+	@Override
+	public void run() {
+		for(int i=0; i<10; i++) {
+			try {
+				Thread.sleep(500); // 현재 스레드의 상태를 block 상태로 변경 
+				System.out.println("Runnable 인터페이스를 이용한 스레드 생성과 작업이 진행됩니다. ");
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} 
+		}
+	}
+
+	
+	
+	
+	
+}
---
+++ javaproject/FirstJava/src/chapter13/Seller.java
@@ -0,0 +1,21 @@
+package chapter13;
+
+public class Seller extends Thread{
+
+	Product product;
+
+	public Seller(Product product) {
+		super();
+		this.product = product;
+	}
+
+	@Override
+	public void run() {
+		product.setProduct("IPhone 14");
+		System.out.println("오늘 판매하는 제품은 " + product.product + "입니다. ");
+	}
+	
+	
+	
+	
+}
