fb17a445529a9690a45ee7ad945428e20951a6cd
jennakim1007 <jennakim1007@gmail.com>
2022-10-13 11:27:32 +0900

    exam 1 풀이

--- sql/select_exam1.sql
+++ sql/select_exam1.sql
@@ -3,10 +3,12 @@
 
 select * from emp;
 
--- 1. 덧셈연산자를 이용하여 모든 사원에 대해서 $300의 급여 인상을 계산한 후 사원의 이름, 급여, 인상된 급여를 출력하시오.
+-- 1. 덧셈연산자를 이용하여 모든 사원에 대해서(행을 찾는 조건이 없다!) $300의 급여 인상을 계산한 후 사원의 이름, 급여, 인상된 급여를 출력하시오.
 
 select ename, sal, sal+300 as newsal
-from emp;
+from emp
+-- 숫자로도 컬럼 데이터를 가져올 수 있음 
+order by 3;
 
 -- 2. 사원의 이름, 급여, 연간 총 수입을 총 수입이 많은 것부터 작은 순으로 출력하시오, 연간 총수입은 월급에 12를 곱한 후 $100의 상여금을 더해서 계산하시오.
 
@@ -48,6 +50,7 @@ where hiredate between '1981-02-20' and '1981-05-01';
 select ename, deptno
 from emp
 where deptno = 20 or deptno = 30
+-- where deptno in(20, 30)
 order by ename desc;
 
 -- 8. 사원의 급여가 2000에서 3000사이에 포함되고 부서번호가 20 또는 30인 사원의 이름, 급여와 부서번호를 출력, 이름순(오름차순)으로 출력하시오.
@@ -56,6 +59,7 @@ select ename, sal, deptno
 from emp
 where sal between 2000 and 3000 and deptno = 20 
 or sal between 2000 and 3000 and  deptno = 30
+-- where sal between 2000 and 3000 and deptno in(20, 30)
 order by ename;
 
 -- 9. 1981년도에 입사한 사원의 이름과 입사일을 출력하시오. (like 연산자와 와일드카드 사용)
@@ -95,6 +99,7 @@ select ename, job, sal
 from emp
 where job = 'CLERK' and sal not in(1600, 950, 1300)
 or job = 'SALESMAN' and sal not in(1600, 950, 1300);
+-- where(job = 'CLERK' or job = 'SALESMAN') and sal not in(1600, 950, 1300);
 ​
 -- 15. 커미션이 $500 이상인 사원의 이름과 급여 및 커미션을 출력하시오.
 
