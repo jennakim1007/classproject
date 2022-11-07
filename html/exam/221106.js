cc38aad6ae45d63e84a1af6e0be2a5432df44763
jennakim1007 <jennakim1007@gmail.com>
2022-11-08 00:02:42 +0900

    exam

--- html/exam/221106.js
+++ html/exam/221106.js
@@ -6,11 +6,13 @@ document.addEventListener('DOMContentLoaded', () => {
     // 4. input type=reset 일 경우 editFormArea(수정) div 보이도록 처리
 
     const userid = document.querySelector('#userID')
+    const pw = document.querySelector('#pw')
     const repw = document.querySelector('#repw')
-    
+    const userName = document.querySelector('#userName')
+    const submitBtn = document.querySelector('#submitBtn')
+    let cnt = 0;
 
     userid.addEventListener('focusout', (event) => {
-
         const check = event.target.value
         let userIDmsg = document.querySelector('#userIDmsg')
 
@@ -21,45 +23,49 @@ document.addEventListener('DOMContentLoaded', () => {
             userIDmsg.textContent = '사용 가능한 아이디입니다.'
             userIDmsg.style.color = 'green';
         }
-
     })
 
     repw.addEventListener('focusout', () => {
-
-        const pw = document.querySelector('#pw')
         let repwMsg = document.querySelector('#repwMsg')
 
-        if(pw.value != repw.value) {
+        if (pw.value != repw.value) {
             repwMsg.textContent = '비밀번호가 일치하지 않습니다.'
             repwMsg.style.color = 'red'
+        } else {
+            repwMsg.textContent = '';
         }
+    })
 
-    }) 
-    
-    const addList = () => {
+    submitBtn.addEventListener('click', () => {
+        const key = cnt++;
 
+        list.innerHTML += `
+            <tr>
+                <td>${key}</td>
+                <td>${userid.value}</td>
+                <td>${pw.value}</td>
+                <td>${userName.value}</td>
+                <td>
+                    <button id="editBtn">수정</button>
+                    <input id="editBtn" type="button" value="등록">
+                    <button id="delBtn">삭제</button>
+                </td>
+            </tr>    
+        `;
+    })
 
+    const delBtn = document.querySelector('#delBtn')
 
+    editBtn.addEventListener('click', () => {
+        const editBtn = document.querySelectorAll('#editBtn');
 
+        alert('');
+    })
 
-
-
-
-
-
-
-
-
-
-
-        
-    }
-
-
-
-
-
-
+    delBtn.addEventListener('click', () => {
+        // delBtn.remove();
+        // delBtn.parentNode.removeChild(); // delBtn 의 부모 삭제 
+    })
 
 
 
