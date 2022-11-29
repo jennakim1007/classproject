ba099576990e59d26e833b0bbc1c969af248e900
jennakim1007 <jennakim1007@gmail.com>
2022-11-29 22:39:01 +0900

    Mapper 재설정

--- spring/intellij/TodoSpring_221127/src/main/java/com/todo/todospring/mapper/MemberMapper.java
+++ spring/intellij/TodoSpring_221127/src/main/java/com/todo/todospring/mapper/MemberMapper.java
@@ -3,19 +3,12 @@ package com.todo.todospring.mapper;
 import com.todo.todospring.domain.MemberDTO;
 import org.apache.ibatis.annotations.Mapper;
 
-
 @Mapper
 public interface MemberMapper {
-
-/*    @Select("select * from member")
-    List<MemberDTO> selectAll();*/
 
     MemberDTO selectbyIdPw(String uid, String pw);
 
     int insertMember(MemberDTO memberDTO);
 
-/*    int deleteMember(MemberDTO memberDTO);
-
-    int updateMember(MemberDTO memberDTO);*/
 
 }
