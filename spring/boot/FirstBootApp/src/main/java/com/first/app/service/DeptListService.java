5769b805c7dbddaec08833066b616a75371eabf9
jennakim1007 <jennakim1007@gmail.com>
2022-12-06 17:34:35 +0900

    Thymeleaf 예제

---
+++ spring/boot/FirstBootApp/src/main/java/com/first/app/service/DeptListService.java
@@ -0,0 +1,26 @@
+package com.first.app.service;
+
+//import com.app.manager.domain.DeptSearchOption;
+//import com.app.manager.mapper.DeptMapper2;
+import com.first.app.domain.DeptDTO;
+import com.first.app.mapper.DeptMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DeptListService {
+
+    @Autowired(required = false)
+    private DeptMapper deptMapper;
+
+    public List<DeptDTO> getList(){
+        return deptMapper.selectAll();
+    }
+
+//    public List<DeptDTO> getSearchList(DeptSearchOption searchOption){
+//        return deptMapper.selectByOption(searchOption);
+//    }
+
+}
